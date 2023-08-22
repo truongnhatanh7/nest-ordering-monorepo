c1980bbaf6ab41760cb77f55eff42fceb67683d2
truongnhatanh7 <truongnhatanhw@gmail.com>
2023-08-22 17:49:18 +0700

    feat: add simple route

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly orderRepository: OrdersRepository) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    return this.orderRepository.create(request);
   }
 }
